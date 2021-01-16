f5a148709bd9fe908e51ffe1ea5822205056e276
Neil de Carteret <neil@lumphammer.com>
2021-01-16 11:02:12 +0000

    extract logo madness

--- src/components/TrailActorSheet.tsx
+++ src/components/TrailActorSheet.tsx
@@ -2,7 +2,8 @@
 import React, { useCallback } from "react";
 import { TrailActor } from "../module/TrailActor";
 import { PoolTracker } from "./PoolTracker";
-import { css, jsx, Global } from "@emotion/react";
+import { css, Global, jsx } from "@emotion/react";
+import { TrailLogo } from "./TrailLogo";
 
 type TrailActorSheetProps = {
   entity: TrailActor,
@@ -41,64 +42,26 @@ export const TrailActorSheet = ({
           "\"title title image\" " +
           "\"pools stats image\" " +
           "\"pools body  body\" ",
+        h1: {
+          border: "none",
+          margin: 0,
+          padding: 0,
+        },
       }}
     >
       <Global
-        // se also
-        // Tenor Sans
-        // Federo
         styles={css`
         @import url('https://fonts.googleapis.com/css2?family=Imbue:wght@300&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Arya:wght@400;700&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Tenor+Sans&display=swap');
-        @import url('https://fonts.googleapis.com/css2?family=Federo&display=swap');
         `}
       />
+
       <div
         css={{
           gridArea: "title",
-          perspective: "1000px",
-          perspectivOrigin: "50% 50%",
         }}
       >
-        <h1
-          css={{
-            position: "relative",
-            display: "inline-block",
-            // fontFamily: "'Arya', sans-serif",
-            // fontFamily: "'Tenor Sans', sans-serif",
-            fontFamily: "'Federo', sans-serif",
-            textTransform: "uppercase",
-            fontSize: "4em",
-            // fontWeight: "bold",
-            letterSpacing: "-0.04em",
-            transform: "scaleX(1) scaleY(1.6) rotateY(-30deg) rotateZ(-1deg) translateX(-10%)",
-            border: "none",
-            margin: "0.5em",
-            padding: 0,
-            lineHeight: 1,
-            ":before": {
-              position: "absolute",
-              top: 0,
-              left: 0,
-              content: "'TRAIL OF CTHULHU'",
-              textShadow: "2px 0px 1px black, 6px 3px 4px rgba(0,0,0,0.5)",
-              color: "transparent",
-              zIndex: -1,
-            },
-            ":after": {
-              position: "absolute",
-              top: 0,
-              left: 0,
-              content: "'TRAIL OF CTHULHU'",
-              background: "linear-gradient(to bottom right, #000, #BE9F00)",
-              backgroundClip: "text",
-              color: "transparent",
-            },
-
-          }}
-        >
-          tRaIl Of CtHuLhU
+        <h1>
+          <TrailLogo/>
         </h1>
       </div>
       <div
