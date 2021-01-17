304aacfddef182803121bca665e93ecb69710bff
Neil de Carteret <neil@lumphammer.com>
2021-01-17 11:46:49 +0000

    document & polish useUpdate hook

--- src/hooks/useUpdate.ts
+++ src/hooks/useUpdate.ts
@@ -1,12 +1,20 @@
 import { useCallback } from "react";
-import { TrailActor } from "../module/TrailActor";
 
-type XForm = (val: string) => any;
+type Transform = (val: string) => any;
 type Callback = (val: string) => void;
 
-export const useUpdate = (entity: TrailActor, xform: XForm): Callback => {
+/**
+ * Hook to create a callback to update a given entity with a given value. The
+ * transform argument should take the one value given to the callback and return
+ * the entity partial to be used for the update.
+ *
+ * Examples:
+ * const updateName = useUpdate(actor, (name) => ({name}))
+ * const updateOccupation = useUpdate(entity, occupation => ({ data: { occupation } }));
+ */
+export const useUpdate = (entity: Entity, transform: Transform): Callback => {
   return useCallback((val: string) => {
-    const update = xform(val);
+    const update = transform(val);
     entity.update(update);
-  }, [entity, xform]);
+  }, [entity, transform]);
 };
