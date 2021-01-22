873da2bb8a0f0ea8856f078ad6f081601bc08d0d
Neil de Carteret <neil@lumphammer.com>
2021-01-22 19:36:40 +0000

    [wip] compendium work

--- imports/generateTrailAbilitiesData.ts
+++ imports/generateTrailAbilitiesData.ts
@@ -94,4 +94,20 @@ const generalAbilities = [
 
 export const generateTrailAbilitiesData = () => {
   console.log(investigativeTemplate, generalTemplate, investigativeAbilities, generalAbilities);
+
+  // Reference a Compendium pack by it's collection ID
+  const pack = game.packs.find(p => p.collection === `${moduleName}.${packName}`);
+
+  // Load an external JSON data file which contains data for import
+  const response = await fetch("worlds/myworld/data/import.json");
+  const content = await response.json();
+
+  // Create temporary Actor entities which impose structure on the imported data
+  const actors = Actor.createMany(content, { temporary: true });
+
+  // Save each temporary Actor into the Compendium pack
+  for (const a of actors) {
+    await pack.importEntity(a);
+    console.log(`Imported Actor ${a.name} into Compendium pack ${pack.collection}`);
+  }
 };
