8e3373099d39746c6dbefa9884997c414b68f896
Neil de Carteret <neil@lumphammer.com>
2021-01-22 19:05:05 +0000

    start work on compendium generator

---
+++ imports/generateTrailAbilitiesData.ts
@@ -0,0 +1,97 @@
+const investigativeTemplate = {
+  type: "investigativeAbility",
+  category: "Academic",
+  hasSpeciality: false,
+  speciality: "",
+  rating: 0,
+  pool: 0,
+};
+
+const generalTemplate = {
+  type: "investigativeAbility",
+  canBeUsedInvestigatively: false,
+  hasSpeciality: false,
+  speciality: "",
+  rating: 0,
+  pool: 0,
+};
+
+const investigativeAbilities = {
+  Academic: [
+    { name: "Accounting" },
+    { name: "Anthropology" },
+    { name: "Archeology" },
+    { name: "Architecture" },
+    { name: "Art History" },
+    { name: "Biology" },
+    { name: "Cthulhu Mythos" },
+    { name: "Cryptography" },
+    { name: "Geology" },
+    { name: "History" },
+    { name: "Language", hasSpeciality: true },
+    { name: "Law" },
+    { name: "Library Use" },
+    { name: "Medicine" },
+    { name: "Occult" },
+    { name: "Physics" },
+    { name: "Theology" },
+  ],
+  Interpersonal: [
+    { name: "Assess Honesty" },
+    { name: "Bargain" },
+    { name: "Bureaucracy" },
+    { name: "Cop Talk" },
+    { name: "Credit Rating" },
+    { name: "Flattery" },
+    { name: "Interrogation" },
+    { name: "Intimidation" },
+    { name: "Oral History" },
+    { name: "Reassurance" },
+    { name: "Streetwise" },
+  ],
+  Technical: [
+    { name: "Art" },
+    { name: "Astronomy" },
+    { name: "Chemistry" },
+    { name: "Craft" },
+    { name: "Evidence Collection" },
+    { name: "Forensics" },
+    { name: "Locksmith" },
+    { name: "Outdoorsman" },
+    { name: "Pharmacy" },
+    { name: "Photography" },
+  ],
+};
+
+const generalAbilities = [
+  { name: "Athletics" },
+  { name: "Conceal" },
+  { name: "Disguise", canBeUsedInvestigatively: true },
+  { name: "Driving" },
+  { name: "Electrical Repair", canBeUsedInvestigatively: true },
+  { name: "Explosives", canBeUsedInvestigatively: true },
+  { name: "Filch" },
+  { name: "Firearms" },
+  { name: "First Aid" },
+  { name: "Fleeing" },
+  { name: "Health" },
+  { name: "Hypnosis" },
+  { name: "Magic" },
+  { name: "Mechanical Repair", canBeUsedInvestigatively: true },
+  { name: "Piloting" },
+  { name: "Preparedness" },
+  { name: "Psychoanalysis" },
+  { name: "Riding" },
+  { name: "Sanity" },
+  { name: "Scuffling" },
+  { name: "Sense Trouble" },
+  { name: "Shadowing" },
+  { name: "Stability" },
+  { name: "Stealth" },
+  { name: "Technical" },
+  { name: "Weapons" },
+];
+
+export const generateTrailAbilitiesData = () => {
+  console.log(investigativeTemplate, generalTemplate, investigativeAbilities, generalAbilities);
+};
