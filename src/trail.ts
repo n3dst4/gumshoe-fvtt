3f033527e04d21710fb2e3a0841b6575d0a87fd9
Neil de Carteret <neil@lumphammer.com>
2021-01-23 13:24:40 +0000

    delete the old hooks code

--- src/trail.ts
+++ src/trail.ts
@@ -6,7 +6,6 @@ import { TrailActorSheetClass } from "./module/TrailActorSheetClass";
 import { TrailItemSheetClass } from "./module/TrailItemSheetClass";
 import { equipment, generalAbility, investigativeAbility } from "./constants";
 import { generateTrailAbilitiesData } from "./generateTrailAbilitiesData";
-// import { isAbility } from "./functions";
 
 // Initialize system
 Hooks.once("init", async function () {
@@ -45,63 +44,6 @@ Hooks.once("setup", function () {
 Hooks.once("ready", function () {
   // Do anything once the system is ready
 });
-
-// function makeName (baseName: string, speciality: string, hasSpeciality: boolean) {
-//   return hasSpeciality ? `${baseName} (${speciality})` : baseName;
-// }
-
-// function updateName (itemData: ItemData<any>, diff: Record<string, any>) {
-//   console.log("!!!preUpdateOwnedItem", diff);
-//   if (
-//     isAbility(itemData.type) &&
-//     diff.data &&
-//     (diff.data.baseName !== undefined ||
-//       diff.data.hasSpeciality !== undefined ||
-//       diff.data.speciality !== undefined)
-//   ) {
-//     const hasSpeciality =
-//       diff.data.hasSpeciality === undefined
-//         ? itemData.data.hasSpeciality
-//         : diff.data.hasSpeciality;
-//     const baseName =
-//       diff.data.baseName === undefined
-//         ? itemData.data.baseName
-//         : diff.data.baseName;
-//     const speciality =
-//       diff.data.speciality === undefined
-//         ? itemData.data.speciality
-//         : diff.data.speciality;
-//     diff.name = makeName(baseName, speciality, hasSpeciality);
-//   }
-// }
-
-// Hooks.on("preUpdateOwnedItem", (actor, itemData, diff, options, actorId) => {
-//   updateName(itemData, diff);
-// });
-// Hooks.on("preUpdateItem", (item, diff, options, actorId) => {
-//   updateName(item.data, diff);
-// });
-
-// Hooks.on("createItem", (item, diff, options, actorId) => {
-//   if (isAbility(item.data.type) && !item.data.data.baseName) {
-//     item.update({
-//       data: {
-//         baseName: item.data.name,
-//       },
-//     });
-//   }
-// });
-
-// XXX revisit this hook when we want to think about creating abilities
-// directly
-// Hooks.on("createOwnedItem", (actor: Actor, itemData, options, actorId) => {
-//   if (isAbility(itemData.type)) {
-//     actor.updateOwnedItem({
-//       ...itemData,
-//       dat
-//     })
-//   }
-// });
 
 // CONFIG.debug.hooks = true;
 
