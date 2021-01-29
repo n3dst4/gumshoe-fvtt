044259ffd62b8e222b85de7743a22f108fb3fa9b
Neil de Carteret <neil@lumphammer.com>
2021-01-29 15:00:19 +0000

    comments

--- src/module/ReactApplicationMixin.tsx
+++ src/module/ReactApplicationMixin.tsx
@@ -1,16 +1,30 @@
 import ReactDOM from "react-dom";
 
+// type shenanigans to allow us to work backwards from a Class type to the type
+// of the objects which it constructs
+
+// a "Constructor of T" is the type of the class T, when used as a value
 export type Constructor<T> = new (...args: any[]) => T;
 
+// so Constructor<Application> is any class which is an Application
 type ApplicationConstuctor = Constructor<Application>;
 
+// Render<T> T is a Constructor<T2>. It then expects its actual argument to be
+// a T2, i.e. the type of the thing the constructor constructs.
 type Render<T> = (t: T extends Constructor<infer T2> ? T2 : T) => JSX.Element;
 
-// This mixin adds a scale property, with getters and setters
-// for changing it with an encapsulated private property:
-
+/**
+ * Wrap an existing Foundry Application class in this Mixin to override the
+ * normal rednering behaviour and and use React instead.
+ */
 export function ReactApplicationMixin<TBase extends ApplicationConstuctor> (
+  /**
+   * The base class.
+   */
   Base: TBase,
+  /** A function which will be given an *instance* of Base and expected to
+   * return some JSX.
+   * */
   render: Render<TBase>,
 ) {
   return class Reactified extends Base {
