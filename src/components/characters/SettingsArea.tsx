0af911fc1dd1e59d7493690199bed87e7afb1ce6
Neil de Carteret <neil@lumphammer.com>
2021-03-06 13:56:44 +0000

    remove commented code

--- src/components/characters/SettingsArea.tsx
+++ src/components/characters/SettingsArea.tsx
@@ -35,18 +35,6 @@ export const SettingArea: React.FC<SettingAreaProps> = ({
             Nuke
           </button>
         </GridField>
-        {/* <GridFieldStacked><hr/></GridFieldStacked>
-        <GridFieldStacked label="Support">
-          <p>
-            If you would like to help me (Neil de Carteret) acquire the e-books
-            of some more GUMSHOE games so I can add support for them to Foundry
-            VTT, you can throw a small amount of money at me on Ko-Fi:
-          </p>
-          <p>
-            <a href="https://ko-fi.com/n3dst4">Buy me a coffee! (well, a bit of an RPG book)</a>
-          </p>
-        </GridFieldStacked>
-        <GridFieldStacked><hr/></GridFieldStacked> */}
     </InputGrid>
   );
 };
