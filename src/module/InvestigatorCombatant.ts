7c9bbac79864f2b2659733225625ec54ebfd13e4
Neil de Carteret <neil@lumphammer.com>
2021-09-15 08:45:37 +0100

    [wip/broken]

--- src/module/InvestigatorCombatant.ts
+++ src/module/InvestigatorCombatant.ts
@@ -1,4 +1,6 @@
+import { ConfiguredDocumentClass } from "@league-of-foundry-developers/foundry-vtt-types/src/types/helperTypes";
 import * as constants from "../constants";
+import { assertPCDataSource, isGeneralAbilityDataSource, isPCDataSource } from "../types";
 import { GumshoeItem } from "./GumshoeItem";
 
 export class InvestigatorCombatant extends Combatant {
@@ -12,9 +14,44 @@ export class InvestigatorCombatant extends Combatant {
       (item: GumshoeItem) => item.type === constants.generalAbility && item.name === abilityName,
     );
     if (ability && ability.data.type === constants.generalAbility) {
-      return ability.data.data.rating.toString();
+      let score = ability.data.data.rating.toString();
+      if (ability.data.data.goesFirstInCombat) {
+        score = `${score} + 100`;
+      }
+      return score;
     } else {
       return "0";
+    }
+  }
+}
+
+export class InvestigatorCombat extends Combat {
+  protected _sortCombatants (
+    a: InstanceType<ConfiguredDocumentClass<typeof Combatant>>,
+    b: InstanceType<ConfiguredDocumentClass<typeof Combatant>>,
+  ): number {
+    if (!(a.actor !== null && b.actor !== null && isPCDataSource(a.actor?.data) && isPCDataSource(b.actor?.data))) {
+      return 0;
+    }
+    // const aData = a.actor.data.data;
+    // const bData = b.actor.data.data;
+    const aAbilityName = a.actor.data.data.initiativeAbility;
+    const aAbility = a.actor.items.find(
+      (item: GumshoeItem) => item.type === constants.generalAbility && item.name === aAbilityName,
+    );
+    const bAbilityName = b.actor.data.data.initiativeAbility;
+    const bAbility = b.actor.items.find(
+      (item: GumshoeItem) => item.type === constants.generalAbility && item.name === bAbilityName,
+    );
+    if (!(aAbility !== undefined && bAbility !== undefined && isGeneralAbilityDataSource(aAbility.data) && isGeneralAbilityDataSource(bAbility.data))) {
+      return 0;
+    }
+    if (aAbility.data.data.goesFirstInCombat && !bAbility.data.data.goesFirstInCombat) {
+      return -1;
+    } else if (!aAbility.data.data.goesFirstInCombat && bAbility.data.data.goesFirstInCombat) {
+      return 1;
+    } else {
+
     }
   }
 }
