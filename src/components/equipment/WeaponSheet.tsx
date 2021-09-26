bb9701da12fe89f18c386bb2114fae3b83f4b136
Neil de Carteret <neil@lumphammer.com>
2021-09-26 13:01:48 +0100

    weapon image

--- src/components/equipment/WeaponSheet.tsx
+++ src/components/equipment/WeaponSheet.tsx
@@ -1,15 +1,16 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GumshoeItem } from "../../module/GumshoeItem";
 import { useAsyncUpdate } from "../../hooks/useAsyncUpdate";
 import { WeaponConfig } from "./WeaponConfig";
 import { WeaponAttack } from "./WeaponAttack";
 import { Translate } from "../Translate";
+import { ImagePickle } from "../ImagePickle";
 
 type WeaponSheetProps = {
   weapon: GumshoeItem,
-  foundryWindow: Application,
+  foundryWindow: ItemSheet,
 };
 
 export const WeaponSheet: React.FC<WeaponSheetProps> = ({
@@ -22,37 +23,68 @@ export const WeaponSheet: React.FC<WeaponSheetProps> = ({
     foundryWindow.render();
   }, [foundryWindow, configMode]);
   return (
-    <Fragment>
-      <div>
+    <div
+      css={{
+        paddingBottom: "1em",
+        display: "grid",
+        gridTemplateColumns: "auto 1fr auto",
+        gridTemplateRows: "auto auto auto",
+        gridTemplateAreas:
+          "\"image slug     cog\" " +
+          "\"image headline headline\" " +
+          "\"body  body     body\" ",
+      }}
+    >
+      <div css={{ gridArea: "slug" }}>
         <Translate>Weapon</Translate>
-        {weapon.isOwned && (
-          <a
-            css={{
-              float: "right",
-            }}
-            onClick={() => {
-              setConfigMode((mode) => !mode);
-            }}
-          >
-            <i className={`fa fa-${configMode ? "check" : "cog"}`} />
-          </a>
-        )}
       </div>
 
+      {weapon.isOwned && (
+        <a
+          css={{
+            gridArea: "cog",
+          }}
+          onClick={() => {
+            setConfigMode((mode) => !mode);
+          }}
+        >
+          <i className={`fa fa-${configMode ? "check" : "cog"}`} />
+        </a>
+      )}
+
       <h1
+        css={{ gridArea: "headline" }}
         contentEditable
         onInput={name.onInput}
         onFocus={name.onFocus}
         onBlur={name.onBlur}
         ref={name.contentEditableRef}
       />
-      {configMode || !weapon.isOwned
-        ? (
-        <WeaponConfig weapon={weapon} />
-          )
-        : (
-        <WeaponAttack weapon={weapon} />
-          )}
-    </Fragment>
+
+      {/* Image */}
+      <ImagePickle
+        editMode={true}
+        document={weapon}
+        application={foundryWindow}
+        css={{
+          gridArea: "image",
+          transform: "rotateZ(-2deg)",
+          width: "4em",
+          height: "4em",
+          margin: "0 1em 0.5em 0",
+        }}
+      />
+
+      <div css={{ gridArea: "body" }}>
+        {configMode || !weapon.isOwned
+          ? (
+          <WeaponConfig weapon={weapon} />
+            )
+          : (
+          <WeaponAttack weapon={weapon} />
+            )
+        }
+      </div>
+    </div>
   );
 };
