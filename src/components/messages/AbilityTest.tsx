107af8833594ec2096befb12537e022e88c464b8
Neil de Carteret <neil@lumphammer.com>
2021-10-23 18:03:54 +0100

    render counting and css reset

--- src/components/messages/AbilityTest.tsx
+++ src/components/messages/AbilityTest.tsx
@@ -1,7 +1,10 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import ReactDOM from "react-dom";
+import { getDefaultThemeName } from "../../settingsHelpers";
+import { themes } from "../../themes/themes";
+import { CSSReset, CSSResetMode } from "../CSSReset";
 // import { assertGame } from "../../functions";
 
 interface RollMessageProps {
@@ -12,19 +15,32 @@ const RollMessage: React.FC<RollMessageProps> = React.memo(({
   msg,
 }) => {
   // assertGame(game);
+  const renderCountRef = useRef(0);
   const [x, setX] = useState(0);
   const incX = useCallback(() => {
     setX((x) => x + 1);
   }, []);
+  const theme = themes[getDefaultThemeName()];
+
+  renderCountRef.current++;
   return (
-    <div>
+    <CSSReset
+      theme={theme}
+      mode={CSSResetMode.small}
+      css={{
+        position: "relative",
+      }}
+    >
       <div>
         Result from React, take 2: {msg.roll?.result}
       </div>
       <div>
         Counter: {x}; <a onClick={incX}>Inc++</a>
       </div>
-    </div>
+      <div>
+        Render count: {renderCountRef.current}
+      </div>
+    </CSSReset>
   );
 });
 
