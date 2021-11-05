6688032673992becbc406a900144d66eec8e8d09
Neil de Carteret <neil@lumphammer.com>
2021-11-05 18:20:37 +0000

    success/failure slugs

--- src/components/messageCards/AbilityTestMwCard.tsx
+++ src/components/messageCards/AbilityTestMwCard.tsx
@@ -12,49 +12,82 @@ interface AbilityTestMwCardProps {
   difficulty: MWDifficulty;
 }
 
-type SuccessOrFailure ="success" | "failure";
-
 interface MWResult {
-  successorFailure: SuccessOrFailure;
   text: string;
   color: string;
+  // css: CSSObject;
 }
 
-export const mainColors: {[x in SuccessOrFailure]: string} = {
-  success: "#a7d5a7",
-  failure: "#d59292",
-};
+const basicShadow = "0 0 0.5em 0 #0007";
 
 const results: {[value: number]: MWResult} = {
   1: {
-    color: "red",
-    successorFailure: "failure",
-    text: "Dismal",
+    text: "Dismal Failure",
+    color: "#fe083f",
+    // css: {
+    //   backgroundColor: "black",
+    //   color: "white",
+    //   textShadow: [
+    //     "0 0 0.5em red",
+    //     "0 0 0.2em red",
+    //     "0 0 1em red",
+    //     "0 0 2em red",
+    //   ].join(","),
+    //   boxShadow: [
+    //     "0 0 1em 0 inset red",
+    //     basicShadow,
+    //   ].join(","),
+    // },
   },
   2: {
-    color: mainColors.failure,
-    successorFailure: "failure",
-    text: "Quotidian",
+    text: "Quotidian Failure",
+    color: "#fd5a00",
+    // css: {
+    //   backgroundColor: "darkred",
+    //   boxShadow: basicShadow,
+    // },
   },
   3: {
-    color: "#ffff81",
-    successorFailure: "failure",
-    text: "Exasperating",
+    text: "Exasperating Failure",
+    color: "#eb8b00",
+    // css: {
+    //   backgroundColor: "pink",
+    //   boxShadow: basicShadow,
+    // },
   },
   4: {
-    color: "#ffff81",
-    successorFailure: "success",
-    text: "Hair’s-Breadth",
+    text: "Hair’s-Breadth Success",
+    color: "#c9b500",
+    // css: {
+    //   backgroundColor: "lightgreen",
+    //   boxShadow: basicShadow,
+    // },
   },
   5: {
-    color: mainColors.success,
-    successorFailure: "success",
-    text: "Prosaic",
+    text: "Prosaic Success",
+    color: "#94d900",
+    // css: {
+    //   backgroundColor: "darkgreen",
+    //   boxShadow: basicShadow,
+    // },
   },
   6: {
-    color: "#0f0",
-    successorFailure: "success",
-    text: "Illustrious",
+    text: "Illustrious Success",
+    color: "#0cf850",
+    // css: {
+    //   backgroundColor: "#333",
+    //   color: "white",
+    //   textShadow: [
+    //     "0 0 0.5em yellow",
+    //     "0 0 0.2em yellow",
+    //     "0 0 1em yellow",
+    //     "0 0 2em yellow",
+    //   ].join(","),
+    //   boxShadow: [
+    //     "0 0 0.5em 0 inset yellow",
+    //     basicShadow,
+    //   ].join(","),
+    // },
   },
 };
 
@@ -144,28 +177,41 @@ export const AbilityTestMwCard: React.FC<AbilityTestMwCardProps> = React.memo(({
       {/* RESULT */}
       <div
         css={{
-          background: mainColors[deets.successorFailure],
           textAlign: "center",
           padding: "0.5em",
-          border: "1px solid #777",
-          borderRadius: "0.5em",
           gridArea: "body",
+          marginTop: "0.5em",
+          borderRadius: "2em",
+          backgroundImage: [
+            `radial-gradient(closest-side, ${deets.color}77 0%, ${deets.color}00 100%)`,
+            "linear-gradient(to bottom, #777, #000)",
+          ].join(", "),
+          boxShadow: basicShadow,
         }}
+      >
+        <div
+          css={{
+            color: "#fff",
+            borderStyle: "solid",
+            borderWidth: "2px",
+            borderColor: "#fff",
+            borderRadius: "1em",
+            textShadow: [
+              `0 0 0.5em ${deets.color}`,
+              `0 0 0.2em ${deets.color}`,
+              `0 0 1em ${deets.color}`,
+              `0 0 2em ${deets.color}`,
+            ].join(","),
+            boxShadow: [
+              `0 0 0.5em 0 inset ${deets.color}`,
+              `0 0 0.5em 0 ${deets.color}`,
+              // basicShadow,
+            ].join(","),
+
+          }}
         >
-          <Translate>
-            {deets.successorFailure === "failure" ? "FailureExclamation" : "SuccessExclamation"}
-          </Translate>
-          <div
-            css={{
-              marginTop: "0.5em",
-              // padding: "1em",
-              background: deets.color,
-              border: "1px solid #777",
-              borderRadius: "0.5em",
-            }}
-          >
-            {deets.text}
-          </div>
+          {deets.text}
+        </div>
       </div>
     </div>
   );
