7c994e84965d31f18bdd574118a56f47ac5e75c9
Neil de Carteret <neil@lumphammer.com>
2021-11-07 15:38:16 +0000

    new ability fields

--- src/components/abilities/AbilityMwExtraFields.tsx
+++ src/components/abilities/AbilityMwExtraFields.tsx
@@ -6,6 +6,7 @@ import { GridField } from "../inputs/GridField";
 import { InputGrid } from "../inputs/InputGrid";
 import { assertGeneralAbilityDataSource } from "../../types";
 import { AsyncTextInput } from "../inputs/AsyncTextInput";
+import { AsyncTextArea } from "../inputs/AsyncTextArea";
 
 type AbilityMwExtraFieldsProps = {
   ability: InvestigatorItem,
@@ -24,6 +25,37 @@ export const AbilityMwExtraFields: React.FC<AbilityMwExtraFieldsProps> = ({
             onChange={ability.setMwTrumps}
           />
       </GridField>
+      <GridField label="Is Trumped or Countered by">
+          <AsyncTextInput
+            value={ability.data.data.mwTrumpedBy}
+            onChange={ability.setMwTrumpedBy}
+          />
+      </GridField>
+      <GridField label="Benefits">
+          <AsyncTextArea
+            value={ability.data.data.mwBenefits}
+            onChange={ability.setMwBenefits}
+          />
+      </GridField>
+      <GridField label="Drawbacks">
+          <AsyncTextArea
+            value={ability.data.data.mwDrawbacks}
+            onChange={ability.setMwDrawbacks}
+          />
+      </GridField>
+      <GridField label="Refreshes">
+          <AsyncTextInput
+            value={ability.data.data.mwRefreshes}
+            onChange={ability.setMwRefreshes}
+          />
+      </GridField>
+      <GridField label="Automatic Success">
+          <AsyncTextInput
+            value={ability.data.data.mwAutomaticSuccess}
+            onChange={ability.setMwAutomaticSuccess}
+          />
+      </GridField>
+
     </InputGrid>
   );
 };
