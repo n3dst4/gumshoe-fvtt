0a8cb93c04cd828b18f8f1af8e792874d0e11a18
Neil de Carteret <neil@lumphammer.com>
2021-11-15 20:13:59 +0000

    text areas refactor

--- src/components/inputs/AsyncTextArea.tsx
+++ src/components/inputs/AsyncTextArea.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import React, { ChangeEvent, useCallback, useContext } from "react";
+import React from "react";
 import { assertGame } from "../../functions";
 import { useAsyncUpdate } from "../../hooks/useAsyncUpdate";
-import { IdContext } from "../IdContext";
+import { TextArea } from "./TextArea";
 
 type AsyncTextAreaProps = {
   className?: string,
@@ -21,8 +21,6 @@ export const AsyncTextArea: React.FC<AsyncTextAreaProps> = ({
   disabled,
   index,
 }) => {
-  const id = useContext(IdContext);
-
   const {
     onChange,
     onFocus,
@@ -30,50 +28,15 @@ export const AsyncTextArea: React.FC<AsyncTextAreaProps> = ({
     display,
   } = useAsyncUpdate(value, onChangeOrig, index);
 
-  const onChangeCb = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
-    onChange(e.currentTarget.value);
-  }, [index, onChange]);
-
-  // inspired by _onDropEditorData in foundry.js
-  const onDropEditorData = async (event : React.DragEvent<HTMLTextAreaElement>) => {
-    event.preventDefault();
-    const data = JSON.parse(event.dataTransfer.getData("text/plain"));
-    if (!data?.id) return;
-
-    // Case 1 - Document from Compendium Pack
-    if (data.pack) {
-      assertGame(game);
-      const pack = game.packs.get(data.pack);
-      if (!pack) return;
-      const document = await pack.getDocument(data.id);
-      const link = `@Compendium[${data.pack}.${data.id}]{${document?.name}}`;
-      onChange(`${display}${link}`);
-    } else if (data.type) {
-      // Case 2 - Document from World
-      const config = CONFIG[data.type as "Actor"|"Item"|"Scene"];
-      if (!config) return false;
-      const entity = (config.collection as any).instance.get(data.id);
-      if (!entity) return false;
-      const link = `@${data.type}[${entity._id}]{${entity.name}}`;
-      onChange(`${display || ""}${link}`);
-    }
-  };
-
   return (
-    <textarea
-      id={id}
-      css={{
-        flex: 1,
-        width: "100%",
-      }}
+    <TextArea
       className={className}
-      data-lpignore="true"
       value={display || ""}
-      onChange={onChangeCb}
+      onChange={onChange}
+
       onFocus={onFocus}
       onBlur={onBlur}
       disabled={disabled}
-      onDrop={(e) => onDropEditorData(e)}
     />
   );
 };
--- src/components/inputs/TextArea.tsx
+++ src/components/inputs/TextArea.tsx
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
 import React, { ChangeEvent, useCallback, useContext } from "react";
+import { assertGame } from "../../functions";
 import { IdContext } from "../IdContext";
 
 type TextAreaProps = {
@@ -32,6 +33,31 @@ export const TextArea: React.FC<TextAreaProps> = ({
     onChange?.(e.currentTarget.value, index);
   }, [index, onChange]);
 
+  // inspired by _onDropEditorData in foundry.js
+  const onDropEditorData = async (event : React.DragEvent<HTMLTextAreaElement>) => {
+    event.preventDefault();
+    const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+    if (!data?.id) return;
+
+    // Case 1 - Document from Compendium Pack
+    if (data.pack) {
+      assertGame(game);
+      const pack = game.packs.get(data.pack);
+      if (!pack) return;
+      const document = await pack.getDocument(data.id);
+      const link = `@Compendium[${data.pack}.${data.id}]{${document?.name}}`;
+      onChange?.(`${value}${link}`, index);
+    } else if (data.type) {
+      // Case 2 - Document from World
+      const config = CONFIG[data.type as "Actor"|"Item"|"Scene"];
+      if (!config) return false;
+      const entity = (config.collection as any).instance.get(data.id);
+      if (!entity) return false;
+      const link = `@${data.type}[${entity._id}]{${entity.name}}`;
+      onChange?.(`${value || ""}${link}`, index);
+    }
+  };
+
   return (
     <textarea
       id={id}
@@ -48,6 +74,7 @@ export const TextArea: React.FC<TextAreaProps> = ({
       onBlur={onBlur}
       disabled={disabled}
       placeholder={placeholder}
+      onDrop={(e) => onDropEditorData(e)}
     />
   );
 };
