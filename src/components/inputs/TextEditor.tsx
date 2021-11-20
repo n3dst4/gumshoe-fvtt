700f87e5fa6844f6bec49a22621568040e767109
Neil de Carteret <neil@lumphammer.com>
2021-11-20 20:07:36 +0000

    start adding md mode

--- src/components/inputs/TextEditor.tsx
+++ src/components/inputs/TextEditor.tsx
@@ -1,7 +1,8 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/react";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useContext, useState } from "react";
 import { assertGame } from "../../functions";
+import { ThemeContext } from "../../themes/ThemeContext";
 import { NoteFormat } from "../../types";
 import { absoluteCover } from "../absoluteCover";
 import { AsyncTextArea } from "./AsyncTextArea";
@@ -24,10 +25,34 @@ export const TextEditor: React.FC<TextEditorProps> = ({
   className,
 }: TextEditorProps) => {
   assertGame(game);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [editMode, setEditMode] = useState(false);
+  const theme = useContext(ThemeContext);
   let editor: ReactNode;
-  if (format === NoteFormat.plain) {
+  if (!editMode) {
+    editor = <div
+      css={{
+        ...absoluteCover,
+        overflow: "auto",
+        background: theme.colors.backgroundPrimary,
+        padding: "0.5em",
+        border: `1px solid ${theme.colors.text}`,
+      }}
+      dangerouslySetInnerHTML={{ __html: html }}
+    />;
+  } else if (format === NoteFormat.plain) {
     editor = (
       <AsyncTextArea
+        key="plain"
+        className={className}
+        onChange={setSource}
+        value={source}
+      />
+    );
+  } else if (format === NoteFormat.markdown) {
+    editor = (
+      <AsyncTextArea
+        key="markdown"
         className={className}
         onChange={setSource}
         value={source}
@@ -49,6 +74,9 @@ export const TextEditor: React.FC<TextEditorProps> = ({
               right: 0,
             }}
           >
+            <a onClick={() => setEditMode((e) => !e)}>
+              <i className="fas fa-edit"/>
+            </a>
             <select value={format} onChange={(e) => setFormat(e.currentTarget.value as NoteFormat)}>
               <option value={NoteFormat.plain}>{game.i18n.localize("investigator.Plain")}</option>
               <option value={NoteFormat.markdown}>{game.i18n.localize("investigator.Markdown")}</option>
