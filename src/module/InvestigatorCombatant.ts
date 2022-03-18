90145100c0738ee30cdb0a732f2601b1409c35e2
Neil de Carteret <neil@lumphammer.com>
2022-03-18 12:04:45 +0000

    more robustness

--- src/module/InvestigatorCombatant.ts
+++ src/module/InvestigatorCombatant.ts
@@ -2,14 +2,22 @@ import * as constants from "../constants";
 import { InvestigatorItem } from "./InvestigatorItem";
 import { assertActiveCharacterDataSource } from "../types";
 import { getCombatAbilities } from "../settingsHelpers";
+import { isNullOrEmptyString } from "../functions";
 /**
  * Override base Combatant class to override the initiative formula.
- * XXX what's i'd like to do is block it from doing a "roll" at all.
+ * XXX what i'd like to do is block it from doing a "roll" at all.
  */
 export class InvestigatorCombatant extends Combatant {
   _getInitiativeFormula () {
     assertActiveCharacterDataSource(this.actor?.data);
+    // get the ability name, and if not set, use the first one on the system
+    // config (we had a bug where some chars were getting created without an
+    // init ability name)
     const abilityName = this.actor?.data.data.initiativeAbility || getCombatAbilities().sort()[0] || "";
+    // and if it was null, set it on the actor now.
+    if (this.actor && isNullOrEmptyString(this.actor.data.data.initiativeAbility)) {
+      this.actor.update({ data: { initiativeAbility: abilityName } });
+    }
     const ability = this.actor.items.find(
       (item: InvestigatorItem) => item.type === constants.generalAbility && item.name === abilityName,
     );
