85ca18588638875d3db6baa86a076680ed624ffd
Neil de Carteret <neil@lumphammer.com>
2022-04-08 19:05:44 +0100

    tidying & commentary

--- src/pipe.test.ts
+++ src/pipe.test.ts
@@ -10,6 +10,7 @@ const v1: V1 = { member1: 1 };
 const v2: V2 = { member2: 2 };
 const v3: V3 = { member3: 3 };
 const v4: V4 = { member4: 4 };
+const v5: V5 = { member5: 5 };
 
 const v1toV2 = (seed: V1): V2 => ({ member2: seed.member1 + 1 });
 const v2toV3 = (seed: V2): V3 => ({ member3: seed.member2 + 1 });
@@ -22,22 +23,25 @@ describe("pipe", () => {
   const v1toV5 = pipe(v1toV2, v2toV5);
 
   test("v1toV2", () => {
-    expect(v1toV2(v1)).toEqual({ member2: 2 });
+    expect(v1toV2(v1)).toEqual(v2);
   });
   test("v2toV3", () => {
-    expect(v2toV3(v2)).toEqual({ member3: 3 });
+    expect(v2toV3(v2)).toEqual(v3);
   });
   test("v3toV4", () => {
-    expect(v3toV4(v3)).toEqual({ member4: 4 });
+    expect(v3toV4(v3)).toEqual(v4);
   });
   test("v4toV5", () => {
-    expect(v4toV5(v4)).toEqual({ member5: 5 });
+    expect(v4toV5(v4)).toEqual(v5);
+  });
+  test("v3toV5", () => {
+    expect(v3toV5(v3)).toEqual(v5);
   });
   test("v2toV5", () => {
-    expect(v2toV5(v2)).toEqual({ member5: 5 });
+    expect(v2toV5(v2)).toEqual(v5);
   });
   test("v1toV5", () => {
-    expect(v1toV5(v1)).toEqual({ member5: 5 });
+    expect(v1toV5(v1)).toEqual(v5);
   });
 });
 
@@ -47,24 +51,24 @@ describe("chainPipe", () => {
   const v1toV5 = chainPipe(v1toV2, v2toV3).pipe(v3toV4).pipe(v4toV5);
 
   test("v1toV2", () => {
-    expect(v1toV2(v1)).toEqual({ member2: 2 });
+    expect(v1toV2(v1)).toEqual(v2);
   });
   test("v2toV3", () => {
-    expect(v2toV3(v2)).toEqual({ member3: 3 });
+    expect(v2toV3(v2)).toEqual(v3);
   });
   test("v3toV4", () => {
-    expect(v3toV4(v3)).toEqual({ member4: 4 });
-  });
-  test("v3toV5", () => {
-    expect(v3toV5(v3)).toEqual({ member5: 5 });
+    expect(v3toV4(v3)).toEqual(v4);
   });
   test("v4toV5", () => {
-    expect(v4toV5(v4)).toEqual({ member5: 5 });
+    expect(v4toV5(v4)).toEqual(v5);
+  });
+  test("v3toV5", () => {
+    expect(v3toV5(v3)).toEqual(v5);
   });
   test("v2toV5", () => {
-    expect(v2toV5(v2)).toEqual({ member5: 5 });
+    expect(v2toV5(v2)).toEqual(v5);
   });
   test("v1toV5", () => {
-    expect(v1toV5(v1)).toEqual({ member5: 5 });
+    expect(v1toV5(v1)).toEqual(v5);
   });
 });
--- src/pipe.ts
+++ src/pipe.ts
@@ -1,15 +1,31 @@
+/**
+ * A pure function of A -> B
+ */
 export type Fn<A, B> = (a: A) => B;
 
+/**
+ * Given two pure functions with compatible out/in types, chaint hem together
+ * and return a new function with is the product of the two.
+ */
 export const pipe = <A, B, C>(
   ab: Fn<A, B>,
   bc: Fn<B, C>,
 ): Fn<A, C> =>
     (a: A) => bc(ab(a));
 
+/**
+ * A simple function of A -> B which also has callable member `pipe`, which can
+ * be used to chain any type-compatible other function, creating a new product
+ * function.
+ */
 export type PipeableFn<A, B> = Fn<A, B> & {
   pipe: <C>(fn: Fn<B, C>) => PipeableFn<A, C>,
 }
 
+/**
+ * Like `pipe`, but the resulting function also has a `pipe` method so you can
+ * chain the pipe calls.
+ */
 export const chainPipe = <A, B, C>(
   ab: ((a: A) => B),
   bc: ((b: B) => C),
