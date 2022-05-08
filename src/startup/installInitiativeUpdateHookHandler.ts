65ce6dfecb0b99c66e045500de25babbe2855072
Neil de Carteret <neil@lumphammer.com>
2022-05-08 15:05:22 +0100

    remove unused hook

--- src/startup/installInitiativeUpdateHookHandler.ts
+++ src/startup/installInitiativeUpdateHookHandler.ts
@@ -1,21 +1,7 @@
 import { assertGame } from "../functions";
 import { InvestigatorCombatant } from "../module/InvestigatorCombatant";
-import { InvestigatorItem } from "../module/InvestigatorItem";
-import { InvestigatorItemDataSource } from "../types";
 
 export function installInitiativeUpdateHookHandler () {
-  Hooks.on("updateItem", function (
-    item: InvestigatorItem,
-    dataDiff: DeepPartial<InvestigatorItemDataSource>,
-    options: unknown,
-    useId: string,
-  ) {
-    assertGame(game);
-    if (game.user?.isGM) {
-      //
-    }
-  });
-
   // on createCombatant, if you are the user wot done it, set the initiative.
   Hooks.on("createCombatant", (combatant: InvestigatorCombatant, options: any, userId: string) => {
     assertGame(game);
