4be30733e7c30ffee9cabbbed662a47c56b866a6
Neil de Carteret <neil@lumphammer.com>
2022-05-16 09:10:07 +0100

    break out hooks from combatant row

--- src/components/combat/StandardCombatantRow.tsx
+++ src/components/combat/StandardCombatantRow.tsx
@@ -1,15 +1,14 @@
 /** @jsx jsx */
 import { cx } from "@emotion/css";
 import { jsx } from "@emotion/react";
-import { ConfiguredObjectClassForName } from "@league-of-foundry-developers/foundry-vtt-types/src/types/helperTypes";
-import React, { Fragment, useCallback, MouseEvent, useRef, ReactNode } from "react";
+import React, { Fragment, ReactNode } from "react";
 import { FaEdit, FaEllipsisH, FaEraser, FaRecycle, FaTrash } from "react-icons/fa";
 import { assertGame } from "../../functions";
-import { useRefStash } from "../../hooks/useRefStash";
 import { InvestigatorCombat } from "../../module/InvestigatorCombat";
 import { Dropdown } from "../inputs/Dropdown";
 import { Menu, MenuItem } from "../inputs/Menu";
 import { InvestigatorTurn } from "./getTurns";
+import { useCombatant } from "./useCombatant";
 
 interface StandardCombatantRowProps {
   turn: InvestigatorTurn;
@@ -23,101 +22,18 @@ export const StandardCombatantRow: React.FC<
   combat,
 }: StandardCombatantRowProps) => {
   assertGame(game);
-  const combatantStash = useRefStash(combat?.combatants.get(turn.id));
-
-  const hoveredToken = useRef<ConfiguredObjectClassForName<"Token"> | null>(
-    null,
-  );
-
-  const onToggleDefeatedStatus = useCallback(async () => {
-    if (combatantStash.current === undefined) return;
-    const isDefeated = !combatantStash.current.isDefeated;
-    await combatantStash.current.update({ defeated: isDefeated });
-    const token = combatantStash.current.token;
-    if (!token) return;
-    // Push the defeated status to the token
-    const status = CONFIG.statusEffects.find(
-      (e) => e.id === CONFIG.Combat.defeatedStatusId,
-    );
-    if (!status && !token.object) return;
-    const effect =
-      token.actor && status ? status : CONFIG.controlIcons.defeated;
-    if (token.object) {
-      await (token.object as Token).toggleEffect(effect, {
-        overlay: true,
-        active: isDefeated,
-      });
-    } else {
-      // @ts-expect-error not sure if fvtt-types is wrong or what
-      await token.toggleActiveEffect(effect, {
-        overlay: true,
-        active: isDefeated,
-      });
-    }
-  }, [combatantStash]);
-
-  const onToggleHidden = () => {
-    return combatantStash.current?.update({ hidden: !combatantStash.current?.hidden });
-  };
-
-  const onDoInitiative = () => {
-    combatantStash.current?.doGumshoeInitiative();
-  };
-
-  const onCombatantHoverIn = useCallback(
-    (event: MouseEvent<HTMLElement>) => {
-      event.preventDefault();
-      if (!canvas?.ready) return;
-      const token = combatantStash.current?.token?.object;
-      // @ts-expect-error isVisible is legit?
-      if (token?.isVisible) {
-        // @ts-expect-error privacy means nothing
-        if (!token._controlled) {
-          // @ts-expect-error privacy means nothing
-          token._onHoverIn(event);
-        }
-        hoveredToken.current =
-          token as unknown as ConfiguredObjectClassForName<"Token">;
-      }
-    },
-    [combatantStash],
-  );
-
-  const onCombatantHoverOut = useCallback((event: MouseEvent<HTMLElement>) => {
-    event.preventDefault();
-    if (hoveredToken.current) {
-      // @ts-expect-error privacy means nothing
-      hoveredToken.current?._onHoverOut(event);
-    }
-    hoveredToken.current = null;
-  }, []);
-
-  const onConfigureCombatant = useCallback(
-    (event: React.MouseEvent<HTMLElement>) => {
-      if (combatantStash.current === undefined) return;
-      const rect = event.currentTarget.getBoundingClientRect();
-      new CombatantConfig(combatantStash.current, {
-        top: Math.min(rect.top, window.innerHeight - 350),
-        left: window.innerWidth - 720,
-        width: 400,
-      }).render(true);
-    },
-    [combatantStash],
-  );
-
-  const onClearInitiative = useCallback(() => {
-    combatantStash.current?.update({ initiative: null });
-  }, [combatantStash]);
-
-  const onRefreshInitiative = useCallback(() => {
-    combatantStash.current?.doGumshoeInitiative();
-  }, [combatantStash]);
-
-  const onRemoveCombatant = useCallback(() => {
-    combatantStash.current?.delete();
-  }, [combatantStash]);
-
-  const localize = game.i18n.localize.bind(game.i18n);
+  const {
+    onToggleDefeatedStatus,
+    onToggleHidden,
+    onDoInitiative,
+    onCombatantHoverIn,
+    onCombatantHoverOut,
+    onConfigureCombatant,
+    onClearInitiative,
+    onRefreshInitiative,
+    onRemoveCombatant,
+    localize,
+  } = useCombatant(combat, turn.id);
 
   return (
     <li
---
+++ src/components/combat/useCombatant.ts
@@ -0,0 +1,117 @@
+import { ConfiguredObjectClassForName } from "@league-of-foundry-developers/foundry-vtt-types/src/types/helperTypes";
+import { useCallback, useRef } from "react";
+import { assertGame } from "../../functions";
+import { useRefStash } from "../../hooks/useRefStash";
+import { InvestigatorCombat } from "../../module/InvestigatorCombat";
+
+export const useCombatant = (combat: InvestigatorCombat|undefined, id: string) => {
+  assertGame(game);
+  const combatantStash = useRefStash(combat?.combatants.get(id));
+
+  const hoveredToken = useRef<ConfiguredObjectClassForName<"Token"> | null>(
+    null,
+  );
+
+  const onToggleDefeatedStatus = useCallback(async () => {
+    if (combatantStash.current === undefined) return;
+    const isDefeated = !combatantStash.current.isDefeated;
+    await combatantStash.current.update({ defeated: isDefeated });
+    const token = combatantStash.current.token;
+    if (!token) return;
+    // Push the defeated status to the token
+    const status = CONFIG.statusEffects.find(
+      (e) => e.id === CONFIG.Combat.defeatedStatusId,
+    );
+    if (!status && !token.object) return;
+    const effect =
+      token.actor && status ? status : CONFIG.controlIcons.defeated;
+    if (token.object) {
+      await (token.object as Token).toggleEffect(effect, {
+        overlay: true,
+        active: isDefeated,
+      });
+    } else {
+      // @ts-expect-error not sure if fvtt-types is wrong or what
+      await token.toggleActiveEffect(effect, {
+        overlay: true,
+        active: isDefeated,
+      });
+    }
+  }, [combatantStash]);
+
+  const onToggleHidden = () => {
+    return combatantStash.current?.update({ hidden: !combatantStash.current?.hidden });
+  };
+
+  const onDoInitiative = () => {
+    combatantStash.current?.doGumshoeInitiative();
+  };
+
+  const onCombatantHoverIn = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      event.preventDefault();
+      if (!canvas?.ready) return;
+      const token = combatantStash.current?.token?.object;
+      // @ts-expect-error isVisible is legit?
+      if (token?.isVisible) {
+        // @ts-expect-error privacy means nothing
+        if (!token._controlled) {
+          // @ts-expect-error privacy means nothing
+          token._onHoverIn(event);
+        }
+        hoveredToken.current =
+          token as unknown as ConfiguredObjectClassForName<"Token">;
+      }
+    },
+    [combatantStash],
+  );
+
+  const onCombatantHoverOut = useCallback((event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (hoveredToken.current) {
+      // @ts-expect-error privacy means nothing
+      hoveredToken.current?._onHoverOut(event);
+    }
+    hoveredToken.current = null;
+  }, []);
+
+  const onConfigureCombatant = useCallback(
+    (event: React.MouseEvent<HTMLElement>) => {
+      if (combatantStash.current === undefined) return;
+      const rect = event.currentTarget.getBoundingClientRect();
+      new CombatantConfig(combatantStash.current, {
+        top: Math.min(rect.top, window.innerHeight - 350),
+        left: window.innerWidth - 720,
+        width: 400,
+      }).render(true);
+    },
+    [combatantStash],
+  );
+
+  const onClearInitiative = useCallback(() => {
+    combatantStash.current?.update({ initiative: null });
+  }, [combatantStash]);
+
+  const onRefreshInitiative = useCallback(() => {
+    combatantStash.current?.doGumshoeInitiative();
+  }, [combatantStash]);
+
+  const onRemoveCombatant = useCallback(() => {
+    combatantStash.current?.delete();
+  }, [combatantStash]);
+
+  const localize = game.i18n.localize.bind(game.i18n);
+
+  return {
+    onToggleDefeatedStatus,
+    onToggleHidden,
+    onDoInitiative,
+    onCombatantHoverIn,
+    onCombatantHoverOut,
+    onConfigureCombatant,
+    onClearInitiative,
+    onRefreshInitiative,
+    onRemoveCombatant,
+    localize,
+  };
+};
