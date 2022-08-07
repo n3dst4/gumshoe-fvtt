12ad2f8a415d32ff394c9f88b5381b2d08386019
Neil de Carteret <git@lumphammer.net>
2022-08-07 20:31:48 +0100

    fix #125 (dropping actors onto party not working)

--- src/startup/installDropActorSheetDataHandler.ts
+++ src/startup/installDropActorSheetDataHandler.ts
@@ -2,13 +2,38 @@ import { InvestigatorActor } from "../module/InvestigatorActor";
 import { party, pc } from "../constants";
 import { assertGame, getFolderDescendants } from "../functions";
 
+// we are in a high fuckiness situation here. I don't have time to work out what
+// the correct types for this DropData are, and there's a breaking change from
+// v9 to v10 where the id is replaced with `uuid` which also includes the
+// document type?
+
+interface DropDataV9 {
+  type: string;
+  id: string;
+  entity?: string;
+}
+
+interface DropDataV10 {
+  type: string;
+  uuid: string;
+  entity?: string;
+}
+
+type DropData = DropDataV9 | DropDataV10;
+
+// fuckiness reaching critical levels here, but we do know for now that we will
+// have either an id or a uuid.
+function getIdFromDropData (dropData: DropData): string {
+  return (dropData as DropDataV9).id || (dropData as DropDataV10).uuid.replace(/^[\w]+\./, "");
+}
+
 export const installDropActorSheetDataHandler = () => {
   Hooks.on(
     "dropActorSheetData",
     (
       targetActor: InvestigatorActor,
       application: Application,
-      dropData: { type: string, id: string, entity?: string },
+      dropData: DropData,
     ) => {
       assertGame(game);
       if (
@@ -19,11 +44,12 @@ export const installDropActorSheetDataHandler = () => {
       ) {
         return;
       }
+      const id = getIdFromDropData(dropData);
       const actorIds =
         dropData.type === "Actor"
-          ? [dropData.id]
+          ? [id]
           : dropData.type === "Folder"
-            ? getFolderDescendants(game.folders?.get(dropData.id))
+            ? getFolderDescendants(game.folders?.get(id))
                 .filter((actor) => {
                   return (actor as any).data.type === pc;
                 })
