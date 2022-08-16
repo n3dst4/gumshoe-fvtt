cc52622500d55520425ab06f673a43b341ddba9d
Neil de Carteret <git@lumphammer.net>
2022-08-16 18:43:36 +0100

    force releases off of main

--- scripts/do-release.sh
+++ scripts/do-release.sh
@@ -2,6 +2,13 @@
 
 set -eux
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$branch" != "main" ]; then
+  echo "You must be on main to release"
+  exit 1
+fi
+
 v=v$(jq .version src/system.json -r)
 git commit -am $v --allow-empty
 git push
