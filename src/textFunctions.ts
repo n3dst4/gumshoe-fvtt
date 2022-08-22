0888f78ee847b9e2e3aad1a0a197773bd7b16d44
Neil de Carteret <git@lumphammer.net>
2022-08-22 18:13:05 +0100

    make turndown async
    kinda pointless but I've done the work now

--- src/textFunctions.ts
+++ src/textFunctions.ts
@@ -1,27 +1,26 @@
 import { escape as escapeText } from "html-escaper";
-import TurndownService from "turndown";
 import { NoteFormat } from "./types";
 import xss from "xss";
+import memoize from "lodash/memoize";
 
-/**
- * Override TurndownService to prevent escaping
- */
-class SafeTurndownService extends TurndownService {
-  // be default, turndown escapes anything that looks like a markdown control
-  // character (like `[` or `#`). I see the logic in this, but actually I feel
-  // like for our purposes, if you have markdown-like html, you actually want to
-  // have that seen as markdown after you convert? Also, it knackers foundry
-  // link codes (which can be rectified, admittedly) and when we use turndown to
-  // generate "plain text" we certainly don't want anything escaped.
-  escape (text: string) {
-    return text;
+const makeTurndownService = memoize(async () => {
+  console.log("Making turndown service");
+  const { default: TurndownService } = await import("turndown");
+  class SafeTurndownService extends TurndownService {
+    // by default, turndown escapes anything that looks like a markdown control
+    // character (like `[` or `#`). I see the logic in this, but actually I feel
+    // like for our purposes, if you have markdown-like html, you actually want to
+    // have that seen as markdown after you convert? Also, it knackers foundry
+    // link codes (which can be rectified, admittedly) and when we use turndown to
+    // generate "plain text" we certainly don't want anything escaped.
+    escape (text: string) {
+      return text;
+    }
   }
-}
-
-/**
- * actual turndownservice object for later use
- */
-const turndownService = new SafeTurndownService();
+  // actual turndownservice object for later use
+  const turndownService = new SafeTurndownService();
+  return turndownService;
+});
 
 // /////////////////////////////////////////////////////////////////////////////
 // Converters
@@ -36,13 +35,14 @@ async function markdownToHtml (markdown: string) {
   return marked(markdown);
 }
 
-export function htmlToMarkdown (html: string) {
+export async function htmlToMarkdown (html: string) {
   // ever first-time a cool regex and then realise you don't need it, but
   // you're so proud of your ninja regex skills dating back to doing Perl in the
   // 90s that you want to leave it in as a comment? Anyway check this bad boy
   // out. Turns out I don't need it because I've overridden turndown to stop it
   // doing any escaping at all.
   // return turndown(source).replace(/@(\w+)\\\[(\w+)\\\]\{([^}]*)\}/g, (m, p1, p2, p3) => `@${p1}[${p2}]{${p3}}`);
+  const turndownService = await makeTurndownService();
   return turndownService.turndown(html);
 }
 
@@ -57,14 +57,14 @@ export async function convertNotes (oldFormat: NoteFormat, newFormat: NoteFormat
     if (oldFormat === NoteFormat.markdown || oldFormat === NoteFormat.plain) {
       newSource = oldSource;
     } else if (oldFormat === NoteFormat.richText) {
-      newSource = htmlToPlaintext(oldSource);
+      newSource = await htmlToPlaintext(oldSource);
     }
     unsafeNewHtml = plainTextToHtml(newSource);
   } else if (newFormat === NoteFormat.markdown) {
     if (oldFormat === NoteFormat.plain || oldFormat === NoteFormat.markdown) {
       newSource = oldSource;
     } else if (oldFormat === NoteFormat.richText) {
-      newSource = htmlToMarkdown(oldSource);
+      newSource = await htmlToMarkdown(oldSource);
     }
     unsafeNewHtml = await markdownToHtml(newSource);
   } else if (newFormat === NoteFormat.richText) {
