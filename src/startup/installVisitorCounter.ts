f1c35422d264a65a180b9973e2deb28724014e00
Neil de Carteret <neil@lumphammer.net>
2022-08-28 19:44:45 +0100

    use dev mode as a way to disable visitor counting

--- src/startup/installVisitorCounter.ts
+++ src/startup/installVisitorCounter.ts
@@ -1,5 +1,10 @@
 import { countSystemVisit } from "@lumphammer/fvtt-visitor-counter-client";
+import { getDevMode } from "../functions";
 
 export const installVisitorCounter = () => {
-  Hooks.on("ready", countSystemVisit);
+  Hooks.on("ready", () => {
+    if (!getDevMode()) {
+      countSystemVisit();
+    }
+  });
 };
