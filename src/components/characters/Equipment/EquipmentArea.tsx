629eed8ab7eb21c255eb0e96eec7d22f12adb316
Neil de Carteret <git@lumphammer.net>
2022-10-17 10:10:41 +0100

    hide uncategorized when empty

--- src/components/characters/Equipment/EquipmentArea.tsx
+++ src/components/characters/Equipment/EquipmentArea.tsx
@@ -35,82 +35,16 @@ export const EquipmentArea: React.FC<EquipmentAreaProps> = ({
           />
         );
       })}
-      <EquipmentCategory
-        actor={actor}
-        categoryId={""}
-        items={uncategorizedItems}
-        name={getTranslated("Uncategorized equipment")}
-        app={app}
-        fields={{}}
-      />
-
-      {/* <div
-        css={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <h2
-          css={{
-            flex: 1,
-            "&&": {
-              margin: "0 0 0 0",
-            },
-          }}
-        >
-          <Translate>Equipment</Translate>
-        </h2>
-        <button
-          css={{
-            flexBasis: "max-content",
-            alignSelf: "flex-start",
-          }}
-          onClick={async () => {
-            await actor.createEmbeddedDocuments("Item", [{
-              type: equipment,
-              name: "New item",
-            }], {
-              renderSheet: true,
-            });
-          }}
-        >
-          <i className="fa fa-plus"/><Translate>Add Equipment</Translate>
-        </button>
-      </div>
-      {items.length === 0 &&
-        <i
-          css={{
-            display: "block",
-            fontSize: "1.2em",
-          }}
-        >
-          <Translate>No equipment yet.</Translate>
-        </i>
-      }
-
-      <div
-        css={{
-          columns: "auto 12em",
-        }}
-      >
-        {
-          sortEntitiesByName(items).map<JSX.Element>((item) => (
-            <a
-              key={item.id}
-              css={{
-                display: "block",
-                position: "relative",
-              }}
-              onClick={() => item.sheet?.render(true)}
-              data-item-id={item.id}
-              onDragStart={onDragStart}
-              draggable="true"
-            >
-              {item.name}
-            </a>
-          ))
-        }
-      </div> */}
+      {uncategorizedItems.length > 0 && (
+        <EquipmentCategory
+          actor={actor}
+          categoryId={""}
+          items={uncategorizedItems}
+          name={getTranslated("Uncategorized equipment")}
+          app={app}
+          fields={{}}
+        />
+      )}
     </div>
   );
 };
