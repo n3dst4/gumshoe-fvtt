b0e135b65d7e20b15f11cb1fd8ec5b765409d88e
Neil de Carteret <git@lumphammer.net>
2023-06-12 15:40:45 +0100

    finish making combat popout resizable

--- src/module/InvestigatorCombatTracker.tsx
+++ src/module/InvestigatorCombatTracker.tsx
@@ -22,13 +22,24 @@ export class InvestigatorCombatTrackerBase extends CombatTracker {
     };
   }
 
+  // we do love a hack. there is a weird behaviour in FVTT where when you pop
+  // out the combat tracker, it gets auto-size some some proportion of the
+  // screen height. This makes it less usable than the normla version. it comes
+  // down to Application#createPopout which doesn't make the popout resizable.
+  // So this override does everything from CombatTracker.createPopout and
+  // Application#createPopout, but adds the `resizable` option.
   createPopout() {
     if ( this._popout ) return this._popout;
-    const pop = new InvestigatorCombatTrackerBase({
+    // I can't see a good way to avoid this circularity, but it's fine.
+    // I SAID IT'S FINE.
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    const pop = new InvestigatorCombatTracker({
       popOut: true,
-      height: null,
+      height: window.innerHeight - 200,
+      // ta-da
       resizable: true,
-    }) as this;
+
+    }) as unknown as this; // the typing is a mess
     this._popout = pop;
     pop._original = this;
     pop.initialize({combat: this.viewed, render: true});
