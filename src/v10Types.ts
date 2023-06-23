978b8e7dc2eb551c6f72cafb52cfff650d4a7a3f
Neil de Carteret <git@lumphammer.net>
2023-06-23 19:53:24 +0100

    replace isObjectEmpty with isEmpty (v10 stuff)

--- src/v10Types.ts
+++ src/v10Types.ts
@@ -260,3 +260,7 @@ export function assertAnyActor(actor: Actor | null): asserts actor is AnyActor {
     throw new Error("not an actor");
   }
 }
+
+declare global {
+  var isEmpty: typeof isObjectEmpty; // eslint-disable-line no-var
+}
