a0b84d1085204aba101f7e4cd87cddea88e7a357
Neil de Carteret <git@lumphammer.net>
2023-10-14 11:19:37 +0100

    typo in small sheet background cover

--- src/themes/olderThanMemoryTheme.ts
+++ src/themes/olderThanMemoryTheme.ts
@@ -19,7 +19,7 @@ export const olderThanMemoryTheme: ThemeV1 = themeFactory({
   smallSheetRootStyle: {
     backgroundImage: `linear-gradient( to right, #0009,#0009), url(systems/${systemId}/assets/wallpaper/red_sands.jpg)`,
     backgroundPosition: "center",
-    backgroundSize: "cpver",
+    backgroundSize: "cover",
   },
   appWindowStyle: {
     boxShadow: `0 0 20px ${glow}`,
