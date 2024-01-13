2edd81a3a441626f454e9a768ee684ba8a69e6d3
Neil de Carteret <git@lumphammer.net>
2024-01-13 16:57:21 +0000

    formatting with prettier

--- src/components/rawJournalSheet/Editor.tsx
+++ src/components/rawJournalSheet/Editor.tsx
@@ -1,4 +1,6 @@
 import MonacoEditor, { Monaco, OnMount } from "@monaco-editor/react";
+import htmlParser from "prettier/plugins/html";
+import prettier from "prettier/standalone";
 import React, { useCallback, useRef } from "react";
 
 interface EditorProps {
@@ -13,20 +15,63 @@ export const Editor: React.FC<EditorProps> = ({ page }) => {
   const editorRef = useRef<IStandalonCodeEditor | null>(null);
 
   function handleEditorWillMount(monaco: Monaco) {
-    monaco.languages.html.htmlDefaults.setOptions({
-      format: {
-        ...monaco.languages.html.htmlDefaults.options.format,
-        tabSize: 2,
-        insertSpaces: true,
-        wrapLineLength: 80,
-        wrapAttributes: "auto",
-      },
-    });
+    // monaco.languages.html.htmlDefaults.setOptions({
+    //   format: {
+    //     ...monaco.languages.html.htmlDefaults.options.format,
+    //     tabSize: 2,
+    //     insertSpaces: true,
+    //     wrapLineLength: 80,
+    //     wrapAttributes: "auto",
+    //   },
+    // });
   }
 
   const handleEditorDidMount: OnMount = useCallback((editor, monaco) => {
     monacoRef.current = monaco;
     editorRef.current = editor;
+    // disable built-in html formatting
+    monaco.languages.html.htmlDefaults.setModeConfiguration({
+      ...monaco.languages.html.htmlDefaults.modeConfiguration,
+      documentFormattingEdits: false,
+      documentRangeFormattingEdits: false,
+    });
+    // use prettier to format html
+    monaco.languages.registerDocumentFormattingEditProvider("html", {
+      async provideDocumentFormattingEdits(model) {
+        const text1 = await prettier.format(model.getValue(), {
+          // wrapAttributes: "force",
+          parser: "html",
+          // plugins: [babel],
+          htmlWhitespaceSensitivity: "ignore",
+          arrowParens: "always",
+          bracketSpacing: true,
+          endOfLine: "lf",
+          insertPragma: false,
+          singleAttributePerLine: false,
+          bracketSameLine: false,
+          printWidth: 80,
+          proseWrap: "preserve",
+          quoteProps: "as-needed",
+          requirePragma: false,
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          // trailingComma: 'es5',
+          useTabs: false,
+          vueIndentScriptAndStyle: false,
+          plugins: [htmlParser],
+        });
+        console.log(1);
+        return [
+          {
+            range: model.getFullModelRange(),
+            text: text1,
+          },
+        ];
+      },
+    });
+
+    // editor.
   }, []);
 
   const doFormat = useCallback(() => {
@@ -91,6 +136,7 @@ export const Editor: React.FC<EditorProps> = ({ page }) => {
           beforeMount={handleEditorWillMount}
           onMount={handleEditorDidMount}
           options={{
+            language: "html",
             automaticLayout: true,
             scrollbar: {
               horizontal: "visible",
