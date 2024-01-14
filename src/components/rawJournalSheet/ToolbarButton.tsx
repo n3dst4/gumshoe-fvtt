aeb714f715ae402659df555e51dfa2e26589c10c
Neil de Carteret <git@lumphammer.net>
2024-01-14 17:14:07 +0000

    fix a weird type import error from react-icons
    I could dive into it more to tbh this is actually better

--- src/components/rawJournalSheet/ToolbarButton.tsx
+++ src/components/rawJournalSheet/ToolbarButton.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { IconType } from "react-icons";
 
 type ToolbarButtonProps = {
   text?: React.ReactNode;
-  icon?: IconType;
+  icon?: React.ComponentType;
   onClick: () => void | Promise<void>;
 };
 
