4a85e0d9e3a156c9461c9b2238e2da6cfa99fc0e
Neil de Carteret <git@lumphammer.net>
2024-01-28 14:16:22 +0000

    fix more tests

--- src/components/rawJournalSheet/documentMemory/createDocumentMemory.test.ts
+++ src/components/rawJournalSheet/documentMemory/createDocumentMemory.test.ts
@@ -1,42 +1,42 @@
 import { describe, expect, test } from "vitest";
 
-import { ArrayOfNulls } from "./ArrayOfNullsEqualityTester";
 import { createDocumentMemory } from "./createDocumentMemory";
+import { DocumentMemory } from "./types";
 
 describe("createDocumentMemory", () => {
   test.each([3, 4, 5, 6, 100, 1000])(
     "creates an empty memory with period %i and no maxDepth",
     (period) => {
       const memory = createDocumentMemory(period);
-      expect(memory).toEqual({
+      expect(memory).toEqual<DocumentMemory>({
         stack: {
-          edits: new ArrayOfNulls(period),
-          snapshot: "",
+          edits: [],
+          bombBay: [],
           next: null,
-          lastPushSnapshot: "",
         },
         serial: 0,
         period,
         maxDepth: null,
-        snapshot: "",
+        snapshots: [],
+        state: "",
       });
     },
   );
   test.each([3, 4, 5, 6, 100, 1000])(
-    "creates an empty memory with period %i and no maxDepth",
+    "creates an empty memory with period %i and maxDepth 10",
     (period) => {
       const memory = createDocumentMemory(period, 10);
-      expect(memory).toEqual({
+      expect(memory).toEqual<DocumentMemory>({
         stack: {
-          edits: new ArrayOfNulls(period),
-          snapshot: "",
+          edits: [],
+          bombBay: [],
           next: null,
-          lastPushSnapshot: "",
         },
         serial: 0,
         period,
         maxDepth: 10,
-        snapshot: "",
+        snapshots: [],
+        state: "",
       });
     },
   );
