89cdf785d0c8df56c7aa2631a5c13392998d1f59
Neil de Carteret <git@lumphammer.net>
2024-02-10 19:28:21 +0000

    start wedging extra css classes into journal viewer

--- src/module/InvestigatorJournalSheet.ts
+++ src/module/InvestigatorJournalSheet.ts
@@ -1,3 +1,6 @@
+import { extraCssClasses, systemId } from "../constants";
+import { systemLogger } from "../functions/utilities";
+
 export class InvestigatorJournalSheet extends JournalSheet {
   /** @override */
   static get defaultOptions() {
@@ -6,14 +9,18 @@ export class InvestigatorJournalSheet extends JournalSheet {
     return options;
   }
 
-  // render() {
-  //   super.render();
-  // }
+  render(...args: any[]) {
+    systemLogger.log("RENDER", args);
+    super.render(...args);
+
+    return this;
+  }
 
   protected _replaceHTML(
     element: JQuery<HTMLElement>,
     html: JQuery<HTMLElement>,
   ): void {
+    systemLogger.log("REPLACE HTML", html);
     super._replaceHTML(element, html);
     // this.
     // html.find(".rollable").click(this._onRoll.bind(this));
@@ -21,7 +28,14 @@ export class InvestigatorJournalSheet extends JournalSheet {
 
   /** @override */
   activateListeners(html: JQuery) {
+    systemLogger.log("ACTIVATE LISTENERS", html);
     super.activateListeners(html);
+    const contentElement = this.element.find(".journal-entry-content");
+    // @ts-expect-error sigh
+    const page = this.document.pages.contents[this._getCurrentPage()];
+    const classes = page.flags[systemId]?.[extraCssClasses] ?? "";
+    contentElement.addClass(classes);
+
     // html.find(".rollable").click(this._onRoll.bind(this));
   }
 
