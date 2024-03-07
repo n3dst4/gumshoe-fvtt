982d09c37306aa9127e53411b87546ccb5449fbb
Neil de Carteret <git@lumphammer.net>
2024-03-07 11:06:32 +0000

    start adding reveal button

--- src/components/inputs/NotesDisplay.tsx
+++ src/components/inputs/NotesDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useLayoutEffect, useMemo, useRef } from "react";
 
 import { FoundryAppContext } from "../../../subtrees/shared-fvtt-bits/src/FoundryAppContext";
 import { assertGame, systemLogger } from "../../functions/utilities";
@@ -6,17 +6,19 @@ import { assertGame, systemLogger } from "../../functions/utilities";
 interface NotesDisplayProps {
   className?: string;
   html: string;
+  revealSecret: (index: number) => void;
 }
 
 export const NotesDisplay: React.FC<NotesDisplayProps> = ({
   className,
   html,
+  revealSecret,
 }) => {
   assertGame(game);
   const application = useContext(FoundryAppContext);
   const user = game.user;
 
-  const showSecrets = useMemo(() => {
+  const isOwner = useMemo(() => {
     let showSecrets = false;
     if (application instanceof DocumentSheet && user) {
       const myLevel = application.document.getUserLevel(user) ?? 0;
@@ -28,15 +30,38 @@ export const NotesDisplay: React.FC<NotesDisplayProps> = ({
     return showSecrets;
   }, [application, user]);
 
-  systemLogger.log("NotesDisplay", { html, showSecrets });
+  systemLogger.log("NotesDisplay", { html, showSecrets: isOwner });
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    ref.current?.querySelectorAll("section.secret").forEach((section, i) => {
+      if (section.querySelector("button[aria-label='Reveal secret']")) {
+        systemLogger.log("Reveal button already exists", i);
+        return;
+      }
+
+      systemLogger.log("Adding reveal button", i);
+      section.classList.contains("revealed");
+      const revealButton = document.createElement("button");
+      // add aria-label to reveal button
+      revealButton.setAttribute("aria-label", "Reveal secret");
+      revealButton.textContent = "Reveal secret";
+      revealButton.addEventListener("click", () => {
+        revealSecret(i);
+      });
+      section.append(revealButton);
+    });
+  }, []);
 
   return (
     <div
+      ref={ref}
       className={className}
       css={{
         minHeight: "100%",
         "section.secret": {
-          display: showSecrets ? "block" : "none",
+          display: isOwner ? "block" : "none",
           ".revealed": {
             display: "block",
           },
