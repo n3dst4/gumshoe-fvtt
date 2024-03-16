6b4d4995405d2ff590e4a0664ebeb46513992e66
Neil de Carteret <git@lumphammer.net>
2024-03-16 19:44:38 +0000

    move subrepo up a level and adjust integrations

--- subs/shared-fvtt-bits/scripts/subtree-push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-git subtree push --prefix=subtrees/shared-fvtt-bits shared-fvtt-bits main
-# execute script relative to this one
-
-$DIR/subtree-pull.sh
--- subs/shared-fvtt-bits/scripts/subtree-split-rejoin.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-git subtree split --prefix=subtrees/shared-fvtt-bits --rejoin
