5cd327b5129f5ffeab7e186f2384bc6cd96804bc
Neil de Carteret <git@lumphammer.net>
2024-03-22 19:57:24 +0000

    rename this because it's not an ambient file

--- shared-fvtt-bits/src/DummyAppV2.tsx
+++ shared-fvtt-bits/src/DummyAppV2.tsx
@@ -1,5 +1,8 @@
+import "./ApplicationV2Types";
+
 import React from "react";
 import { createRoot, Root } from "react-dom/client";
+
 import { RecursivePartial } from "./types";
 
 interface DummyAppV2ComponentProps extends React.PropsWithChildren {}
