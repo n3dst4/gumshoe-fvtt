beb98fe49100c32f2961c32de6333a0c92400f63
Neil de Carteret <git@lumphammer.net>
2024-06-15 16:09:11 +0100

    tidy up installDropActorSheetDataHandler

--- src/startup/installDropActorSheetDataHandler.ts
+++ src/startup/installDropActorSheetDataHandler.ts
@@ -2,34 +2,20 @@ import { party, pc } from "../constants";
 import { assertGame, getFolderDescendants } from "../functions/utilities";
 import { InvestigatorActor } from "../module/InvestigatorActor";
 
-// we are in a high fuckiness situation here. I don't have time to work out what
-// the correct types for this DropData are, and there's a breaking change from
-// v9 to v10 where the id is replaced with `uuid` which also includes the
-// document type?
-
-interface DropDataV9 {
-  type: string;
-  id: string;
-  entity?: string;
-}
-
-interface DropDataV10 {
+// this isn't the full type for DropData but it's close enough for what we need
+interface DropData {
   type: string;
   uuid: string;
   entity?: string;
 }
 
-type DropData = DropDataV9 | DropDataV10;
-
-// fuckiness reaching critical levels here, but we do know for now that we will
-// have either an id or a uuid.
 function getIdFromDropData(dropData: DropData): string {
-  return (
-    (dropData as DropDataV9).id ||
-    (dropData as DropDataV10).uuid.replace(/^[\w]+\./, "")
-  );
+  return dropData.uuid.replace(/^[\w]+\./, "");
 }
 
+/**
+ * This is how we drop actors onto the party sheet.
+ */
 export const installDropActorSheetDataHandler = () => {
   Hooks.on(
     "dropActorSheetData",
@@ -51,14 +37,11 @@ export const installDropActorSheetDataHandler = () => {
       const actorIds =
         dropData.type === "Actor"
           ? [id]
-          : dropData.type === "Folder"
-            ? getFolderDescendants(game.folders?.get(id))
-                .filter((actor) => {
-                  return (actor as any).type === pc;
-                })
-                .map((actor) => (actor as any).id)
-            : [];
-
+          : getFolderDescendants(game.folders?.get(id))
+              .filter((actor) => {
+                return (actor as any).type === pc;
+              })
+              .map((actor) => (actor as any).id);
       void targetActor.addActorIds(actorIds);
     },
   );
