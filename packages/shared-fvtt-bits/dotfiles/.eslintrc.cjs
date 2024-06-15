428e8069fa6d5c0a7b6829b156a0ddc0e86d4ed1
Neil de Carteret <git@lumphammer.net>
2024-06-15 16:25:40 +0100

    commentary on all eslint rules customizations

--- packages/shared-fvtt-bits/dotfiles/.eslintrc.cjs
+++ packages/shared-fvtt-bits/dotfiles/.eslintrc.cjs
@@ -38,6 +38,7 @@ module.exports = {
           "Import `systemLogger` from `functions` instead.",
       },
     ],
+    // replaced by ts version
     "no-useless-constructor": "off",
     // need to replace this with @typescript-eslint/no-restricted-imports so we
     // can allow type imports from lodash but this will require some eslint etc
@@ -48,10 +49,20 @@ module.exports = {
 
     //
     // TS
+
+    // optional, but a nice bit of rigor
     "@typescript-eslint/no-use-before-define": ["error"],
+
+    // unfortunately foundry create way too many situation where we need to talk
+    // about any
     "@typescript-eslint/no-explicit-any": ["off"],
-    // XXX ?
+
+    // this would be a huge pain to enable because there's no auto-fix
     "@typescript-eslint/explicit-module-boundary-types": ["off"],
+
+    // we have many situations where we need to supply a function to a  Foundry
+    // API and it feels more correct to able to declare the args we're getting
+    // even if we don't use them all.
     "@typescript-eslint/no-unused-vars": [
       "error",
       { args: "none", ignoreRestSiblings: true },
@@ -59,7 +70,10 @@ module.exports = {
     // stylistic
     "@typescript-eslint/no-empty-function": "off",
 
+    // we need to use global namespaces for league types
     "@typescript-eslint/no-namespace": ["warn", { allowDeclarations: true }],
+
+    // I just have OPINIONS about this one
     "@typescript-eslint/member-delimiter-style": [
       "error",
       {
@@ -85,18 +99,36 @@ module.exports = {
         },
       },
     ],
+    // we also have noPropertyAccessFromIndexSignature: true in tsconfig which
+    // effectively forces this on. See
+    // https://typescript-eslint.io/rules/dot-notation#allowindexsignaturepropertyaccess
     "@typescript-eslint/dot-notation": [
       "error",
-      // we also have noPropertyAccessFromIndexSignature: true in tsconfig which
-      // effectively forces this on. See
-      // https://typescript-eslint.io/rules/dot-notation#allowindexsignaturepropertyaccess
       { allowIndexSignaturePropertyAccess: true },
     ],
 
+    // I tried https://typescript-eslint.io/rules/no-unnecessary-condition/ but
+    // it causes more problems than it solves - for example indexed access in TS
+    // always returns the value type of the thing being access, but sometimes
+    // you want to check for undefined. With that rule on, you either need to
+    // * make the container's value type be whatever|undefined
+    //   * then either check for that everywhere, whereas in fact it's only
+    //    sometimes that you want the check)
+    //   * or you litter `!` everywhere.
+    // * or you put /*esling-ignore*/ everywhere.
+    // either way, not nice.
+
     //
     // react
+
+    // this isn't smart enough to to see the type param given to `React.FC<...>`
     "react/prop-types": ["off"],
+    // if there's a better way to tell eslint about emotion's `css` props, I'd
+    // love to know
     "react/no-unknown-property": ["error", { ignore: ["css"] }],
+
+    //
+    // simple-import-sort
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
   },
