c11ef5681fef96c26aeee742f275a96a2ecbef40
Neil de Carteret <git@lumphammer.net>
2024-07-01 20:28:33 +0100

    route outlets based on step and route

--- packages/minirouter/src/outlets/useOutletRoute.ts
+++ packages/minirouter/src/outlets/useOutletRoute.ts
@@ -1,10 +1,20 @@
-import { ReactNode, useContext, useEffect, useId } from "react";
+import { ReactNode, useContext, useEffect, useId, useMemo } from "react";
 
+import { useNavigationContext } from "../useNavigationContext";
 import { OutletContext } from "./OutletContext";
 
 export const useOutletRoute = (content: ReactNode) => {
-  const id = useId();
+  const routeId = useId();
   const outletContext = useContext(OutletContext);
+  const { currentStep } = useNavigationContext();
+  const stepId = currentStep?.id ?? "";
+
+  // the effective id of this route has to be a combination of the step id and
+  // an additional id for the route itself. If we just use the step is, we can't
+  // differentiate between multiple routes, and if we just use the route id, we
+  // can't differentiate between multiple (presumbaly different params) steps.
+  const id = useMemo(() => `${stepId}-${routeId}`, [routeId, stepId]);
+
   useEffect(() => {
     if (outletContext) {
       outletContext.register(id, content);
@@ -12,6 +22,6 @@ export const useOutletRoute = (content: ReactNode) => {
         outletContext.unregister(id);
       };
     }
-  }, [content, id, outletContext]);
+  }, [content, outletContext, id]);
   return outletContext ? null : content;
 };
