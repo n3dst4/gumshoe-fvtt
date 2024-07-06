92fc18b88fa1a2ff7066efeef59945e15964d30c
Neil de Carteret <git@lumphammer.net>
2024-07-06 18:20:12 +0100

    re-enable cards and MCB
    reinstate card objects

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -10,13 +10,13 @@ import { CSSReset } from "../CSSReset";
 import { TabContainer } from "../TabContainer";
 import { Translate } from "../Translate";
 import { AbilitySettings } from "./AbilitySettings";
-// import { CardsSettings } from "./Cards/CardsSettings";
+import { CardsSettings } from "./Cards/CardsSettings";
 import { DirtyContext, DispatchContext, StateContext } from "./contexts";
 import { CoreSettings } from "./CoreSettings";
 import { EquipmentSettings } from "./Equipment/EquipmentSettings";
 import { useSettingsState } from "./hooks";
 import { MiscSettings } from "./MiscSettings";
-// import { ExampleApp } from "./router/Example";
+import { ExampleApp } from "./router/Example";
 import { StatsSettings } from "./Stats/StatsSettings";
 
 type SettingsProps = {
@@ -101,22 +101,22 @@ export const Settings: React.FC<SettingsProps> = ({ foundryApplication }) => {
                     label: "Stats",
                     content: <StatsSettings />,
                   },
-                  // {
-                  //   id: "cards",
-                  //   label: "Cards",
-                  //   content: <CardsSettings setters={setters} />,
-                  // },
+                  {
+                    id: "cards",
+                    label: "Cards",
+                    content: <CardsSettings setters={setters} />,
+                  },
                   {
                     id: "misc",
                     label: "Misc",
                     content: <MiscSettings setters={setters} />,
                   },
-                  // {
-                  //   id: "router",
-                  //   label: "Router",
-                  //   content: <ExampleApp />,
-                  //   translate: false,
-                  // },
+                  {
+                    id: "router",
+                    label: "Router",
+                    content: <ExampleApp />,
+                    translate: false,
+                  },
                 ]}
               />
             </div>
