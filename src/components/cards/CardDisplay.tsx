c156c8c5d6498ab7ac03ca06623e26d935d7506b
Neil de Carteret <git@lumphammer.net>
2024-08-07 17:49:43 +0100

    rudiments of card display

---
+++ src/components/cards/CardDisplay.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+
+import { ThemeContext } from "../../themes/ThemeContext";
+import { assertCardItem, CardItem } from "../../v10Types";
+
+interface CardDisplayProps {
+  card: CardItem;
+}
+
+export const CardDisplay: React.FC<CardDisplayProps> = ({ card }) => {
+  assertCardItem(card);
+  const theme = useContext(ThemeContext);
+  return (
+    <div
+      css={{
+        ...theme.smallSheetRootStyle,
+        border: "1px solid black",
+        aspectRatio: "4/5",
+      }}
+    >
+      <h2>{card.name}</h2>
+      <p>{card.system.notes.html}</p>
+    </div>
+  );
+};
+
+CardDisplay.displayName = "Card";
