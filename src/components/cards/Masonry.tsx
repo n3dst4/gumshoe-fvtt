de4a0ab11ae0fc965210e5a1bde0f99372706d2e
Neil de Carteret <git@lumphammer.net>
2024-08-09 18:25:30 +0100

    [very wip] masonry

--- src/components/cards/Masonry.tsx
+++ src/components/cards/Masonry.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { ReactNode, useLayoutEffect } from "react";
 
-import { absoluteCover } from "../absoluteCover";
 import { useElementSize } from "./useElementSize";
 
 export type RenderComponentProps<TData> = {
@@ -12,24 +11,74 @@ export type RenderComponentProps<TData> = {
 interface MasonryProps<TData> {
   data: TData[];
   render: (props: RenderComponentProps<TData>) => React.ReactNode;
+  minColumnWidth?: number;
 }
 
 export const createMasonry = <TData,>() => {
   const Masonry = React.memo<MasonryProps<TData>>(function Masonry({
     data,
     render,
+    minColumnWidth = 170,
   }) {
     const containerRef = React.useRef<HTMLDivElement>(null);
     const [width] = useElementSize(containerRef);
-    const children = data.map((datum, index) => {
-      return (
-        <div className="masonry-item-wrapper" key={index}>
-          {render({ index, data: datum, width })}
-        </div>
+
+    useLayoutEffect(() => {
+      //
+    }, []);
+
+    const columnCount = 1;
+    const columnHeights = new Array<number>(columnCount).fill(0);
+    const dataHeights = new Array(data.length).fill(0);
+    let children: ReactNode = null;
+
+    if (containerRef.current) {
+      const columnCount = Math.max(Math.floor(width / minColumnWidth), 1);
+      const columnWidth = width / columnCount;
+
+      const columnarData = new Array<Array<number>>(columnCount).fill([]);
+
+      data.forEach((datum, index) => {
+        const height = dataHeights[index];
+        const shortestColumnIndex = columnHeights.indexOf(
+          Math.min(...columnHeights),
+        );
+        console.log(shortestColumnIndex, columnHeights[shortestColumnIndex]);
+        columnHeights[shortestColumnIndex] += height;
+        columnarData[shortestColumnIndex].push(index);
+      });
+
+      children = columnarData.map((indicesinColumn, columnIndex) =>
+        indicesinColumn.map((indexInData, indexInColumn) => {
+          return (
+            <div
+              key={indexInData}
+              className="masonry-item-wrapper"
+              css={{
+                position: "absolute",
+              }}
+            >
+              {render({
+                index: indexInData,
+                data: data[indexInData],
+                width: columnWidth,
+              })}
+            </div>
+          );
+        }),
       );
-    });
+    }
+
     return (
-      <div className="masonry" css={{}} ref={containerRef}>
+      <div
+        className="masonry"
+        css={{
+          width: "100%",
+          height: Math.max(...columnHeights) + 200 + "px",
+          overflow: "hidden",
+        }}
+        ref={containerRef}
+      >
         {children}
       </div>
     );
