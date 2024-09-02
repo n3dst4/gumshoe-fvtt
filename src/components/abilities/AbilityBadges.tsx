b8aa4a45a1d496a597c0be7f82849cde4399a16f
Neil de Carteret <git@lumphammer.net>
2024-09-02 19:42:10 +0100

    flag push pools on sheet

--- src/components/abilities/AbilityBadges.tsx
+++ src/components/abilities/AbilityBadges.tsx
@@ -2,7 +2,8 @@ import React, { ReactNode, useContext } from "react";
 
 import { InvestigatorItem } from "../../module/InvestigatorItem";
 import { ThemeContext } from "../../themes/ThemeContext";
-import { assertAbilityItem } from "../../v10Types";
+import { assertAbilityItem, isGeneralAbilityItem } from "../../v10Types";
+import { Translate } from "../Translate";
 import { SituationalModifierBadge } from "./SituationalModifierBadge";
 
 interface UnlockBadgesProps {
@@ -22,7 +23,7 @@ export const AbilityBadges: React.FC<UnlockBadgesProps> = ({
     <div className={className}>
       {/* Boost */}
       {ability.system.boost && (
-        <span
+        <Translate
           css={{
             background: theme.colors.accentContrast,
             color: theme.colors.accent,
@@ -35,7 +36,23 @@ export const AbilityBadges: React.FC<UnlockBadgesProps> = ({
           }}
         >
           Boosted
-        </span>
+        </Translate>
+      )}
+      {isGeneralAbilityItem(ability) && ability.system.isPushPool && (
+        <Translate
+          css={{
+            background: theme.colors.accentContrast,
+            color: theme.colors.accent,
+            outline: `1px solid ${theme.colors.accent}`,
+            fontSize: "0.9em",
+            lineHeight: "1",
+            borderRadius: "0.5em",
+            padding: "0 0.5em",
+            margin: "0 0.25em",
+          }}
+        >
+          Push Pool
+        </Translate>
       )}
       {/* Situational modifers */}
       <div
