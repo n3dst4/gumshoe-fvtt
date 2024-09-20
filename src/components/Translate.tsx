924c3cf9c9522c53f15ebbd73e140e54958a9e5f
Neil de Carteret <git@lumphammer.net>
2024-09-21 00:36:24 +0100

    memoise translate

--- src/components/Translate.tsx
+++ src/components/Translate.tsx
@@ -2,7 +2,7 @@ import Case from "case";
 import React, { useMemo } from "react";
 
 import { systemId } from "../constants";
-import { assertGame, getDevMode } from "../functions/utilities";
+import { assertGame, getDevMode, systemLogger } from "../functions/utilities";
 import { settings } from "../settings/settings";
 
 type TranslateProps = {
@@ -12,34 +12,34 @@ type TranslateProps = {
   className?: string;
 };
 
-export const Translate: React.FC<TranslateProps> = ({
-  children,
-  values,
-  title,
-  className,
-}) => {
-  assertGame(game);
-  const debug = settings.debugTranslations.get() && getDevMode();
-  const pascal = useMemo(() => Case.pascal(children), [children]);
-  const prefixed = `${systemId}.${pascal}`;
-  const local = useMemo(() => {
+export const Translate = React.memo<TranslateProps>(
+  ({ children, values, title, className }) => {
     assertGame(game);
-    return game.i18n.format(prefixed, values);
-  }, [prefixed, values]);
-  const has = useMemo(() => {
-    assertGame(game);
-    return game.i18n.has(prefixed, false);
-  }, [prefixed]);
+    systemLogger.log("Translate", children, values);
+    const debug = settings.debugTranslations.get() && getDevMode();
+    const pascal = useMemo(() => Case.pascal(children), [children]);
+    const prefixed = `${systemId}.${pascal}`;
+    const local = useMemo(() => {
+      assertGame(game);
+      return game.i18n.format(prefixed, values);
+    }, [prefixed, values]);
+    const has = useMemo(() => {
+      assertGame(game);
+      return game.i18n.has(prefixed, false);
+    }, [prefixed]);
 
-  return (
-    <span
-      className={className}
-      title={title ?? (debug ? prefixed : local)}
-      style={{
-        background: debug ? (has ? "lightgreen" : "red") : "none",
-      }}
-    >
-      {local}
-    </span>
-  );
-};
+    return (
+      <span
+        className={className}
+        title={title ?? (debug ? prefixed : local)}
+        style={{
+          background: debug ? (has ? "lightgreen" : "red") : "none",
+        }}
+      >
+        {local}
+      </span>
+    );
+  },
+);
+
+Translate.displayName = "Translate";
