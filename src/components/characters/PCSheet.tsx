53e5dcbca8aea05b14dbc0418b8f3d8316ff7b16
Muwak <ingo@cthulhu.at>
2024-10-08 18:19:47 +0200

    no message

--- src/components/characters/PCSheet.tsx
+++ src/components/characters/PCSheet.tsx
@@ -1,45 +1,11 @@
-import React, {
-  Fragment,
-  ReactNode,
-  useCallback,
-  useEffect,
-  useState,
-} from "react";
+import React from "react";
 
-import { occupationSlotIndex } from "../../constants";
 import { assertGame } from "../../functions/utilities";
-import { useTheme } from "../../hooks/useTheme";
 import { InvestigatorActor } from "../../module/InvestigatorActor";
-import { settings } from "../../settings/settings";
 import {
-  AnyItem,
-  assertPCActor,
-  isPCActor,
-  PersonalDetailItem,
+  assertPCActor,  
 } from "../../v10Types";
 // import CardsArea from "../cards/CardsArea";
-import { CSSReset } from "../CSSReset";
-import { ImagePickle } from "../ImagePickle";
-import { AsyncTextInput } from "../inputs/AsyncTextInput";
-import { CombatAbilityDropDown } from "../inputs/CombatAbilityDropDown";
-import { GridField } from "../inputs/GridField";
-import { IndexedAsyncTextInput } from "../inputs/IndexedAsyncTextInput";
-import { InputGrid } from "../inputs/InputGrid";
-import { TabContainer } from "../TabContainer";
-import { Translate } from "../Translate";
-import { AbilitiesAreaEdit } from "./AbilitiesAreaEdit";
-import { AbilitiesAreaPlay } from "./AbilitiesAreaPlay";
-import { EquipmentArea } from "./Equipment/EquipmentArea";
-import { LogoEditable } from "./LogoEditable";
-import { AbilitiesAreaMW } from "./MoribundWorld/AbilitiesAreaMW";
-import { MwInjuryStatusWidget } from "./MoribundWorld/MwInjuryStatusWidget";
-import { MwItemArea } from "./MoribundWorld/MwItemArea";
-import { NotesArea } from "./NotesArea";
-import { PersonalDetailField } from "./PersonalDetailField";
-import { SettingArea } from "./SettingsArea";
-import { StatField } from "./StatField";
-import { TrackersArea } from "./TrackersArea";
-import { WeaponsArea } from "./Weapons/WeaponsArea";
 import { PCSheetFull } from "./PCSheetFull";
 import { PCSheetSimple } from "./PCSheetSimple";
 interface PCSheetProps {
--- src/components/characters/PCSheetSimple.tsx
+++ src/components/characters/PCSheetSimple.tsx
@@ -1,47 +1,20 @@
-import React, {
-  Fragment,
-  ReactNode,
-  useCallback,
+import React, {    
   useEffect,
   useState,
 } from "react";
 
-import { occupationSlotIndex } from "../../constants";
 import { assertGame } from "../../functions/utilities";
 import { useTheme } from "../../hooks/useTheme";
 import { InvestigatorActor } from "../../module/InvestigatorActor";
-import { settings } from "../../settings/settings";
 import {
   AnyItem,
   assertPCActor,
-  isPCActor,
   PersonalDetailItem,
 } from "../../v10Types";
 // import CardsArea from "../cards/CardsArea";
 import { CSSReset } from "../CSSReset";
 import { ImagePickle } from "../ImagePickle";
-
 import { LogoEditable } from "./LogoEditable";
-import { InputGrid } from "../inputs/InputGrid";
-import { GridField } from "../inputs/GridField";
-import { AsyncTextInput } from "../inputs/AsyncTextInput";
-import { PersonalDetailField } from "./PersonalDetailField";
-import { IndexedAsyncTextInput } from "../inputs/IndexedAsyncTextInput";
-import { Translate } from "../Translate";
-import { MwInjuryStatusWidget } from "./MoribundWorld/MwInjuryStatusWidget";
-import { TrackersArea } from "./TrackersArea";
-import { StatField } from "./StatField";
-import { CombatAbilityDropDown } from "../inputs/CombatAbilityDropDown";
-import { TabContainer } from "../TabContainer";
-import { AbilitiesAreaMW } from "./MoribundWorld/AbilitiesAreaMW";
-import { AbilitiesAreaPlay } from "./AbilitiesAreaPlay";
-import { MwItemArea } from "./MoribundWorld/MwItemArea";
-import { WeaponsArea } from "./Weapons/WeaponsArea";
-import { EquipmentArea } from "./Equipment/EquipmentArea";
-import { NotesArea } from "./NotesArea";
-import { AbilitiesAreaEdit } from "./AbilitiesAreaEdit";
-import { SettingArea } from "./SettingsArea";
-
 
 interface PCSheetProps {
   actor: InvestigatorActor;
@@ -55,21 +28,8 @@ export const PCSheetSimple: React.FC<PCSheetProps> = ({
   assertGame(game);
   assertPCActor(actor);
 
-  const updateShortNote = useCallback(
-    async (value: string, index: number) => {
-      await actor.setShortNote(index, value);
-    },
-    [actor],
-  );
 
-  const updateMwHiddenShortNote = useCallback(
-    async (value: string, index: number) => {
-      await actor.setMwHiddenShortNote(index, value);
-    },
-    [actor],
-  );
 
-  const genericOccupation = settings.genericOccupation.get();
 
   const [occupation, setOccupation] = useState<PersonalDetailItem | undefined>(
     actor.getOccupations()[0],
@@ -95,10 +55,6 @@ export const PCSheetSimple: React.FC<PCSheetProps> = ({
 
   const themeName = actor.getSheetThemeName();
   const theme = useTheme(themeName);
-  const personalDetails = settings.personalDetails.get();
-  const shortHiddenNotesNames = settings.mwHiddenShortNotes.get();
-  const occupationLabel = settings.occupationLabel.get();
-  const stats = settings.pcStats.get();
 
   return (  <CSSReset
     theme={theme}
